82f7672a98b2c52797c1317ed9ed787b1d71b671
Hiro-hase <bumphasehiro@gmail.com>
2019-12-20 17:48:50 +0900

    課題提出-変数・定数

---
+++ variable/src/variable/task1.java
@@ -0,0 +1,5 @@
+double a = 31.2;
+string b = "Java研修";
+int c = 9;
+int d = 2147483648;
+char e = '才';
---
+++ variable/src/variable/task3.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class task3 {
+
+	public static void main(String[] args) {
+		int a = 219;
+		long b = a;
+		System.out.println(b);
+
+		double c = 364.2;
+		float d = (float) c;
+		System.out.println(d);
+
+		long e = 123456L;
+		double f = e;
+		System.out.println(f);
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
