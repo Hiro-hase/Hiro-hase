9fde0d6625402dc8d83ab2c660cc0d4e95075217
Hiro-hase <bumphasehiro@gmail.com>
2019-12-23 18:06:00 +0900

    条件分岐-課題提出

--- branch/src/branch/task6.java
+++ branch/src/branch/task6.java
@@ -6,21 +6,23 @@ public class task6 {
 		Scanner sc = new Scanner(System.in);
 		int numA = sc.nextInt();
 		sc.close();
+		String str;
+		if(numA == 1) {
+			str =  numA + "->" + "Ⅰ";
+		}else if(numA == 2) {
+			str = numA + "->" + "Ⅱ";
+		}else if(numA == 3) {
+			str = numA + "->" + "Ⅲ";
+		}else if(numA == 4) {
+			str = numA + "->" + "Ⅳ";
+		}else if(numA == 5) {
+			str = numA + "->" + "Ⅴ";
+		}else{
+			str = numA + "->" + "unknow";
+		}
 		System.out.println("数字を入力してください");
 		System.out.println(numA);
-		if(numA > 5) {
-			System.out.println( numA + "->" + "unknown");
-		}else if(numA == 1) {
-			System.out.println( numA + "->" + "Ⅰ");
-		}else if(numA == 2) {
-			System.out.println( numA + "->" + "Ⅱ");
-		}else if(numA == 3) {
-			System.out.println( numA + "->" + "Ⅲ");
-		}else if(numA == 4) {
-			System.out.println( numA + "->" + "Ⅳ");
-		}else if(numA == 5) {
-			System.out.println( numA + "->" + "Ⅴ");
-		}
+		System.out.println(str);
 
 	}
 
--- branch/src/branch/task7.java
+++ branch/src/branch/task7.java
@@ -6,21 +6,29 @@ public class task7 {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
 		sc.close();
-		if(str == "月曜") {
-			System.out.println(str);
-			System.out.println("午前診療と午後診療があります");
-		}else if(str == "土曜"){
-			System.out.println(str);
-			System.out.println("土曜は午前診療のみです");
-		}else if(str == "日曜"){
-			System.out.println(str);
-			System.out.println("日曜は休診です");
-		}else{
-			System.out.println(str);
-			System.out.println("入力を確認してください");
+		System.out.println("病院に行く曜日を入力してください");
+		System.out.println("【月曜,火曜,水曜,木曜,金曜,土曜,日曜】");
+		System.out.println(str);
+		switch(str) {
+			case "月曜" :
+			case "火曜" :
+			case "水曜" :
+			case "木曜" :
+			case "金曜" :
+				System.out.println("午前診療と午後診療があります");
+				break;
+			case "土曜" :
+				System.out.println("土曜は午前休診です");
+				break;
+			case "日曜" :
+				System.out.println("日曜は休診です");
+				break;
+			default:
+				System.out.println("入力を確認してください");
+				break;
+
 
 		}
-
 
 	}
 
