0f2cc97a850307a4aab047c404da95b9c7ebbd57
Hiro-hase <bumphasehiro@gmail.com>
2020-02-10 11:27:46 +0900

    再提出

--- variable/src/variable/task1.java
+++ variable/src/variable/task1.java
@@ -4,7 +4,7 @@ public class task1 {
 
 	public static void main(String[] args) {
 		double a = 31.2;
-		string b = "Java研修";
+		String b = "Java研修";
 		int c = 9;
 		long d = 2147483648L;
 		char e = '才';
