b07801034faff095d8ae09ab5467c1054381886d
Hiro-hase <bumphasehiro@gmail.com>
2020-02-10 11:49:08 +0900

    再提出

--- variable/src/variable/task3.java
+++ variable/src/variable/task3.java
@@ -6,15 +6,15 @@ public class task3 {
 		int intNum = 219;
 		long b = intNum;
 
-		double bleNum = 364.2;
-		float d = (float) bleNum;
+		double doubleNum = 364.2;
+		float d = (float) doubleNum;
 
 		long longNum = 123456L;
 		double f = longNum;
 
 		System.out.println("変換前");
 		System.out.println(intNum);
-		System.out.println(bleNum);
+		System.out.println(doubleNum);
 		System.out.println(longNum);
 		System.out.println("変換後");
 		System.out.println(b);
